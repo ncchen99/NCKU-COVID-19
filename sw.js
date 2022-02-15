4d1484ce7a0f086cdd38fe75aee519373b60d991
ncchen99 <ncchen99@gmail.com>
2022-02-15 20:57:19 +0800

    💚

--- sw.js
+++ sw.js
@@ -7,3 +7,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   console.log("小屋歸🐢");
 });
+
+this.addEventListener("fetch", function (event) {
+  // it can be empty if you just want to get rid of that error
+});

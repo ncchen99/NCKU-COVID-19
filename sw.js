35c9e447686acd9e02d4f01c00c7a8b3350230b2
ncchen99 <ncchen99@gmail.com>
2022-02-20 22:41:13 +0800

    🥗

--- sw.js
+++ sw.js
@@ -3,11 +3,16 @@ self.addEventListener("install", (event) => {
   console.log("installing…");
 });
 
-// activate
-self.addEventListener("activate", (event) => {
-  console.log("小屋歸🐢");
+// Service Worker-based solution
+self.addEventListener("activate", async () => {
+  // after we've taken over, iterate over all the current clients (windows)
+  const tabs = await self.clients.matchAll({ type: "window" });
+  tabs.forEach((tab) => {
+    // ...and refresh each one of them
+    tab.navigate(tab.url);
+    console.log("小屋歸🐢");
+  });
 });
-
 this.addEventListener("fetch", function (event) {
   console.log("喔喔喔");
   // it can be empty if you just want to get rid of that error
